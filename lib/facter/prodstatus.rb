a70ac7e844e3f9cc630acfbb12c990fb36f2fbea
Hannes Doyle <hannes.doyle@ericsson.com>
2016-07-15 14:09:05 +0200

    Check if File exists else NA

--- lib/facter/prodstatus.rb
+++ lib/facter/prodstatus.rb
@@ -2,11 +2,19 @@ Facter.add('production_state') do
   fcv = Facter.value(:facterversion).split('.')
   if fcv[0] == '1'
     setcode do
-      Facter::Util::Resolution.exec('cat /etc/prodstatus/state')
+      if File.exist? '/etc/prodstatus/state'
+        Facter::Util::Resolution.exec('cat /etc/prodstatus/state')
+      else
+        'NA'
+      end
     end
   else
     setcode do
-      Facter::Core::Execution.exec('cat /etc/prodstatus/state')
+      if File.exist? '/etc/prodstatus/state'
+        Facter::Core::Execution.exec('cat /etc/prodstatus/state')
+      else
+        'NA'
+      end
     end
   end
 end
@@ -14,11 +22,19 @@ Facter.add('production_type') do
   fcv = Facter.value(:facterversion).split('.')
   if fcv[0] == '1'
     setcode do
-      Facter::Util::Resolution.exec('cat /etc/prodstatus/type')
+      if File.exist? '/etc/prodstatus/type'
+        Facter::Util::Resolution.exec('cat /etc/prodstatus/type')
+      else
+        'NA'
+      end
     end
   else
     setcode do
-      Facter::Core::Execution.exec('cat /etc/prodstatus/type')
+      if File.exist? '/etc/prodstatus/type'
+        Facter::Core::Execution.exec('cat /etc/prodstatus/type')
+      else
+        'NA'
+      end
     end
   end
 end
