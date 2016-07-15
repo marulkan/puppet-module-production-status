7116da013d74f8d32f7110e8a3a8c45b8df0d460
Hannes Doyle <hannes.doyle@ericsson.com>
2016-07-15 12:48:04 +0200

    Added backwards compability for facter
    Should now work on facter versions prior to 2.0.0

--- lib/facter/prodstatus.rb
+++ lib/facter/prodstatus.rb
@@ -1,10 +1,24 @@
 Facter.add('production_state') do
-  setcode do
-    Facter::Core::Execution.exec('cat /etc/prodstatus/state')
+  fcv = :facterversion.split('.')
+  if fcv[0] == '1'
+    setcode do
+      Facter::Util::Resolution.exec('cat /etc/prodstatus/state')
+    end
+  else
+    setcode do
+      Facter::Core::Execution.exec('cat /etc/prodstatus/state')
+    end
   end
 end
 Facter.add('production_type') do
-  setcode do
-    Facter::Core::Execution.exec('cat /etc/prodstatus/type')
+  fcv = :facterversion.split('.')
+  if fcv[0] == '1'
+    setcode do
+      Facter::Util::Resolution.exec('cat /etc/prodstatus/type')
+    end
+  else
+    setcode do
+      Facter::Core::Execution.exec('cat /etc/prodstatus/type')
+    end
   end
 end
